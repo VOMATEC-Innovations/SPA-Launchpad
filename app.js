4e9d0aed1e72f88ac7744a6730445ef2697ec5d5
t-knapp <tobias-knapp@web.de>
2021-07-11 23:24:34 +0200

    Metadata and code style.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 const configEnvPrefix = 'SPA_';
 let config = {};
@@ -8,17 +8,16 @@ let config = {};
 app.use(express.static('public'));
 
 app.get('/config', (req, res) => {
-    res.set('Content-Type', 'application/json')
+    res.set('Content-Type', 'application/json');
     res.send(JSON.stringify(config));
 })
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
+    console.log(`Listening at http://localhost:${port}`);
     console.log(`Building config`);
-    for (const [key, value] of Object.entries(process.env))
+    for (const [key, value] of Object.entries(process.env));
         if (key.startsWith(configEnvPrefix))
             config[key.replace(configEnvPrefix, '')] = value;
 
     console.log('Config', JSON.stringify(config));
 })
-
