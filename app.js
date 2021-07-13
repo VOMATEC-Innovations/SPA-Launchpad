980bb36f390857dd2ba93006ca1b7f9bbe802f82
Tobias Knapp <tobias.knapp@vomatec.de>
2021-07-13 16:55:41 +0200

    Config route is configurable.

--- app.js
+++ app.js
@@ -2,22 +2,36 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const configEnvPrefix = 'SPA_';
-let config = {};
+
+const buildConfigData = () => {
+    const config = {}
+    const configEnvPrefix = 'SPA_';
+    for (const [key, value] of Object.entries(process.env))
+    if (key.startsWith(configEnvPrefix))
+        config[key.replace(configEnvPrefix, '')] = value;
+    
+    return config;
+}
+const configData = buildConfigData();
+
+const getConfigRoute = () => {
+    let route = process.env.CONFIG_ROUTE;
+    if (route)
+        route = route.replace(/[^a-zA-Z0-9-_]/g, '');
+
+    return route ? `/${route}` : '/config';
+}
+const configRoute = getConfigRoute();
+
 
 app.use(express.static('public'));
 
-app.get('/config', (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.send(JSON.stringify(config));
+app.get(configRoute, (req, res) => {
+    res.json(configData);
 })
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
-    console.log(`Building config`);
-    for (const [key, value] of Object.entries(process.env));
-        if (key.startsWith(configEnvPrefix))
-            config[key.replace(configEnvPrefix, '')] = value;
-
-    console.log('Config', JSON.stringify(config));
+    console.log(`Serving config on route '${configRoute}'`);
+    console.log('Config:', JSON.stringify(configData));
 })
